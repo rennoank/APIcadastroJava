1d52bf069df93b78a7b79ee4cdbd9103e045e13c
rennoan <129307709+rennoank@users.noreply.github.com>
2025-05-08 02:14:14 -0300

    Add files via upload

---
+++ src/main/java/io/github/rennoank/Cadastro/de/Usuario/CadastroDeUsuarioApplication.java
@@ -0,0 +1,13 @@
+package io.github.rennoank.Cadastro.de.Usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rennoank/Cadastro/de/Usuario/CadastroDeUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rennoank.Cadastro.de.Usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
